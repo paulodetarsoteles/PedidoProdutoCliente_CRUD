6ae6b822c2f07deed4a6c0a53f728f845d5036ba
Paulo <paulodetarso.teles@hotmail.com>
2025-05-04 11:13:14 -0300

    Aplicação do bloco try-cath no DataSeeder das migrations

--- PedidoProdutoCliente.API/Extensions/DataSeeder.cs
+++ PedidoProdutoCliente.API/Extensions/DataSeeder.cs
@@ -6,12 +6,19 @@ namespace PedidoProdutoCliente.API.Extensions
     {
         public static IApplicationBuilder UseDataSeeder<TDbContext>(this IApplicationBuilder app) where TDbContext : DbContext
         {
+            try
+            {
             using var scope = app.ApplicationServices.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
 
             dbContext.Database.Migrate();
 
             return app;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro na aplicação das migrations: {ex.Message}");
+            }
         }
     }
 }
